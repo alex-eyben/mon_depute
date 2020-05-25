a0e3e4d46892ea4ec000842705dc440813c513af
Madeline Grammatico <grammaticomadeline@hotmail.fr>
2020-05-25 18:15:33 +0200

    setup seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Law.destroy_all
+
+puts "Creating laws..."
+lois  = { title: "Dishoom", 
+            content: "7 Boundary St, London E2 7JE", 
+            ressource_link: 5, 
+            current_status: 5, 
+            last_status_update: 5, 
+            start_date: 5,
+            created_at: 5,
+            updated_at: 5 }
+pizza_east =  { title: "Dishoom", 
+                content: "7 Boundary St, London E2 7JE", 
+                ressource_link: 5, 
+                current_status: 5, 
+                last_status_update: 5, 
+                start_date: 5,
+                created_at: 5,
+                updated_at: 5  
+              }
+
+[ dishoom, pizza_east ].each do |attributes|
+  law = Law.create!(attributes)
+  puts "Created #{law.title}"
+end
+puts "Finished!
