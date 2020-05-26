82dbf63655d5e7d43a91b50b52c54aa2038e1126
victor holl <v_h@me.com>
2020-05-26 17:02:43 +0200

    parse into deputies files OK up to 25 deputies

--- app/jobs/import_deputies_job.rb
+++ app/jobs/import_deputies_job.rb
@@ -6,40 +6,45 @@ require 'json'
 class ImportDeputiesJob < ApplicationJob
   queue_as :default
 
-  # def initialize
-    # @url = "http://data.assemblee-nationale.fr/static/openData/repository/15/amo/deputes_actifs_mandats_actifs_organes/AMO10_deputes_actifs_mandats_actifs_organes_XV.json.zip"
-  # end
-
   def perform(quantity)
     puts "coucou"
-    test(quantity)
-    return "all ok"
+    parse_deputies_files(quantity)
   end
 
-  def test(quantity)
+  def parse_deputies_files(quantity)
     @url = "http://data.assemblee-nationale.fr/static/openData/repository/15/amo/deputes_actifs_mandats_actifs_organes/AMO10_deputes_actifs_mandats_actifs_organes_XV.json.zip"
     file = open(@url)
     deputies = []
     Zip::File.open(file) do |zip_file|
-      zip_file.first(quantity).each do |entry|
+      zip_file.first(quantity).each_with_index do |entry, index|
         data = JSON.parse(entry.get_input_stream.read)
-        # deputy = {}
+        deputy = {}
         # p data["acteur"]["uid"]["@xsi:type"]
         # p data["acteur"]["uid"]["#text"]
-        # deputy[:first_name] = data["acteur"]["etatCivil"]["ident"]["prenom"]
-        # deputy[:last_name] = data["acteur"]["etatCivil"]["ident"]["nom"]
-        # deputy[:birth_date] = data["acteur"]["etatCivil"]["infoNaissance"]["dateNais"].to_datetime
-        # puts JSON.pretty_generate(data["acteur"]["etatCivil"])
-        deputies << data["acteur"]["mandats"]["mandat"][-1]["election"]["refCirconscription"]
+        deputy[:first_name] = data["acteur"]["etatCivil"]["ident"]["prenom"]
+        deputy[:last_name] = data["acteur"]["etatCivil"]["ident"]["nom"]
+        deputy[:birth_date] = data["acteur"]["etatCivil"]["infoNaissance"]["dateNais"].to_date
+        deputy[:birth_infos] = data["acteur"]["etatCivil"]["infoNaissance"]
+        deputy[:birth_place] = data['acteur']['etatCivil']['infoNaissance']['villeNais']
+        deputy[:email] = data["acteur"]["adresses"]["adresse"].select{|address|address["typeLibelle"]=="Mèl"}.first["valElec"]
+        deputy[:adresses_elec] = data['acteur']['adresses']['adresse'][1..-1].map{|address| address = { address["typeLibelle"] => address["valElec"]} }
+        # deputy[:other_adresses] = data["acteur"]["adresses"]["adresse"].reject{|address|address["typeLibelle"]=="Mèl"}
+        deputy[:job] = data['acteur']['profession']["libelleCourant"]
+        deputy[:revenue] = data['acteur']["uri_hatvp"]
+        deputy[:party] = data["acteur"]["mandats"]["mandat"].select{|mandat|mandat["typeOrgane"]=="PARPOL"}.first["uid"]
+        deputy[:circonscription] = [data["acteur"]["mandats"]["mandat"].last["election"]["lieu"]["numDepartement"], data["acteur"]["mandats"]["mandat"].last["election"]["lieu"]["numCirco"]]
+        # puts JSON.pretty_generate(data["acteur"]["mandats"]["mandat"].last["election"]["lieu"])
+        deputies << deputy
         # p deputy
+        p "INDEX : #{index}"
       end
-      p deputies.size
-      p deputies.uniq.size
-      puts deputies.size == deputies.uniq.size
+      puts JSON.pretty_generate(deputies)
+      # p deputies.size
+      # p deputies.uniq.size
+      # puts deputies.size == deputies.uniq.size
+      puts "All OK"
+      return deputies
     end
   end
 
 end
-
-
-# amadou = { first_name: "Aude", last_name: "Amadou", email: "aude.amadou@assemblee-nationale.fr", job: "Ex-sportive de haut niveau", birth_place: "Coutances", birth_date: Time.now.to_datetime, party: "LREM", twitter: "https://twitter.com/AudeAmadou", facebook: "AmadouAude", website: "www.aude-amadou.info", revenue: 45000, circonscription: 4 }
