3e4030f546d59a2fa0fef5ff86dcfc221f9a040b
victor holl <v_h@me.com>
2020-05-26 16:03:34 +0200

    just a prettify of deputies jsons

---
+++ app/jobs/pretty_deputy_json_job.rb
@@ -0,0 +1,20 @@
+require 'zip'
+require 'open-uri'
+require 'pry-byebug'
+require 'json'
+
+class PrettyDeputyJsonJob < ApplicationJob
+  queue_as :default
+
+  def perform(quantity)
+    @url = "http://data.assemblee-nationale.fr/static/openData/repository/15/amo/deputes_actifs_mandats_actifs_organes/AMO10_deputes_actifs_mandats_actifs_organes_XV.json.zip"
+    file = open(@url)
+    Zip::File.open(file) do |zip_file|
+      zip_file.first(quantity).each do |entry|
+        data = JSON.parse(entry.get_input_stream.read)
+        puts JSON.pretty_generate(data)
+      end
+      "youpi"
+    end
+  end
+end
