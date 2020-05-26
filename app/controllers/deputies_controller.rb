3ebdcce5fa35497a2b1a864ab9ccce5b51852f02
Alexandra Eyben <alexeyben@hotmail.com>
2020-05-26 16:50:19 +0200

    show deputy setup

--- app/controllers/deputies_controller.rb
+++ app/controllers/deputies_controller.rb
@@ -1,5 +1,6 @@
 class DeputiesController < ApplicationController
   def show
     @deputy = Deputy.find(params[:id])
+    @votes = @deputy.votes
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,26 +25,26 @@ deputies = []
 end
 
 puts "Creating laws..."
-lois_du_11_mai = { title: "Lois du 11 mai", 
-            content: "Présenté au Conseil des ministres du 2 mai 2020 par Édouard Philippe, 
+lois_du_11_mai = { title: "Loi du 11 mai",
+            content: "Présenté au Conseil des ministres du 2 mai 2020 par Édouard Philippe,
             Premier ministre, le projet de loi avait été adopté en première lecture
-             avec modifications par le Sénat le 5 mai et par l'Assemblée nationale le 8 mai 2020. 
-             Après accord en commission mixte paritaire, le texte avait été définitivement adopté 
+             avec modifications par le Sénat le 5 mai et par l'Assemblée nationale le 8 mai 2020.
+             Après accord en commission mixte paritaire, le texte avait été définitivement adopté
              par l'Assemblée nationale et par le Sénat le 9 mai.
-            Le 2 mai, le gouvernement a engagé la procédure accélérée.", 
-            ressource_link: "https://vie-publique.fr/loi/274230-loi-du-11-mai-2020-prolongation-etat-durgence-sanitaire", 
-            current_status: "adoptée", 
-            last_status_update: Date.today, 
+            Le 2 mai, le gouvernement a engagé la procédure accélérée.",
+            ressource_link: "https://vie-publique.fr/loi/274230-loi-du-11-mai-2020-prolongation-etat-durgence-sanitaire",
+            current_status: "adoptée",
+            last_status_update: Date.today,
             start_date:  Date.today
             }
-loi_du_13_mai =  { title: "Lois du 13 mai", 
-                content: "La Commission invite les pays de l'UE et ceux associés à 
-                l'espace Schengen (Islande, Liechtenstein, Norvège, Suisse) à s’engager à lever 
-                progressivement les contrôles aux frontières intérieures. 
-                Elle propose également une approche par étapes pour rétablir des déplacements sans restrictions.", 
-                ressource_link: "https://www.vie-publique.fr/en-bref/274361-retour-progressif-la-libre-circulation-des-personnes", 
-                current_status: "en cours", 
-                last_status_update:  Date.today, 
+loi_du_13_mai =  { title: "Loi du 13 mai",
+                content: "La Commission invite les pays de l'UE et ceux associés à
+                l'espace Schengen (Islande, Liechtenstein, Norvège, Suisse) à s’engager à lever
+                progressivement les contrôles aux frontières intérieures.
+                Elle propose également une approche par étapes pour rétablir des déplacements sans restrictions.",
+                ressource_link: "https://www.vie-publique.fr/en-bref/274361-retour-progressif-la-libre-circulation-des-personnes",
+                current_status: "en cours",
+                last_status_update:  Date.today,
                 start_date:  Date.today
               }
 
