26310325d971000f02acf47c29b01cd03e644e29
Madeline Grammatico <grammaticomadeline@hotmail.fr>
2020-05-29 11:21:40 +0200

    footer and dasboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'laws/:id', to: 'laws#show'
   get 'votes/:id', to: 'votes#show'
+  get '/dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :deputies, only: [:show] do
     collection do
