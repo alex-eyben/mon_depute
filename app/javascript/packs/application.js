3a6dcc5e36486bb3020ee37108655ef8ec168d3e
Ugo Castellazi <ugo.castellazzi@gmail.com>
2020-06-01 18:31:25 +0200

    first integration of progress bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,9 +28,11 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initAutocomplete } from '../plugins/init_autocomplete';
+import { initProgressbar } from '../plugins/init_progressbar';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initAutocomplete();
+  initProgressbar();
 });
