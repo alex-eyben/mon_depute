eda28ea4cedf43fb055edff8a163e7035930dfda
Ugo Castellazi <ugo.castellazzi@gmail.com>
2020-06-02 14:56:29 +0200

    all working, need to add tag list on unfiltered view

--- app/javascript/plugins/init_progressbar.js
+++ app/javascript/plugins/init_progressbar.js
@@ -36,7 +36,7 @@ const initProgressbar = () => {
               },
               autoStyleContainer: false
             },
-            from: {color: '#2B7178'},
+            from: {color: '#E94F37'},
             to: {color: '#2B7178'},
             step: (state, bar) => {
               bar.setText(Math.round(bar.value() * 100) + ' %');
@@ -67,7 +67,7 @@ const initProgressbar = () => {
               },
               autoStyleContainer: false
             },
-            from: {color: '#2B7178'},
+            from: {color: '#E94F37'},
             to: {color: '#2B7178'},
             step: (state, bar) => {
               bar.setText(Math.round(bar.value() * 100) + ' %');
@@ -77,34 +77,36 @@ const initProgressbar = () => {
     
     
           const filteredAttendanceRate = document.querySelector(".filtered-participation-rate").innerText
-          const filter = new ProgressBar.Line(filtered, {
-            strokeWidth: 4,
-            easing: 'easeInOut',
-            duration: 1400,
-            color: '#2B7178',
+          const filter = new ProgressBar.SemiCircle(filtered, {
+            strokeWidth: 6,
+            color: '#FFEA82',
             trailColor: '#eee',
             trailWidth: 1,
-            svgStyle: {width: '100%', height: '100%'},
+            easing: 'easeInOut',
+            duration: 1400,
+            svgStyle: null,
             text: {
-              style: {
-                // Text color.
-                // Default: same as stroke color (options.color)
-                color: '#999',
-                position: 'absolute',
-                right: '0',
-                top: '30px',
-                padding: 0,
-                margin: 0,
-                transform: null
-              },
-              autoStyleContainer: false
+              value: '',
+              alignToBottom: true
             },
-            from: {color: '#2B7178'},
+            from: {color: '#E94F37'},
             to: {color: '#2B7178'},
+            // Set default step function for all animate calls
             step: (state, bar) => {
-              bar.setText(Math.round(bar.value() * 100) + ' %');
+              bar.path.setAttribute('stroke', state.color);
+              const value = Math.round(bar.value() * 100);
+              if (value === 0) {
+                bar.setText('');
+              } else {
+                bar.setText(value + "%");
+              }
+          
+              bar.text.style.color = state.color;
             }
           });
+          filter.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+          filter.text.style.fontSize = '1.5rem';
+          
           filter.animate(filteredAttendanceRate);
 
     };
