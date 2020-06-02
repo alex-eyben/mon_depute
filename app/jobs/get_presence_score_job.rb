b71d45b3c5b14b58054c6dad6fd3d1c39fb9b98d
victor holl <v_h@me.com>
2020-06-02 15:56:43 +0200

    added to import positions

--- app/jobs/get_presence_score_job.rb
+++ app/jobs/get_presence_score_job.rb
@@ -8,7 +8,7 @@ class GetPresenceScoreJob < ApplicationJob
   end
 
   def compute(deputies)
-    print "\r#{((@counter.fdiv(@total))*100).round(2)}%" + " ...Computing fronding score..." + ['/', '-', '\\'].sample
+    print "\r#{((@counter.fdiv(@total))*100).round(2)}%" + " ...Computing presence score..." + ['/', '-', '\\'].sample
     @counter += 1
     deputies.each{|deputy| deputy.update(presence: (deputy.positions.where(votant: true).size.fdiv(deputy.positions.size)*100).round(0))}
   end
