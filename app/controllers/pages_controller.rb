8bc387196de5430f1bcf6e61726b88e499efa7d7
Madeline Grammatico <grammaticomadeline@hotmail.fr>
2020-06-03 10:51:50 +0200

    fix wall of shame

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,6 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    before_action :authenticate_user!
     @user = current_user
     deputy_array = @user.votes.for_type(Deputy)
     ids = []
