b079b9711a28414d8c9af82776c39a815ac648f2
victor holl <v_h@me.com>
2020-06-03 10:55:02 +0200

    fixed

--- app/jobs/get_presence_score_job.rb
+++ app/jobs/get_presence_score_job.rb
@@ -2,14 +2,14 @@ class GetPresenceScoreJob < ApplicationJob
   queue_as :default
 
   def perform(deputies)
-    @counter = 1
     @total = deputies.size
     compute(deputies)
   end
 
   def compute(deputies)
-    print "\r#{((@counter.fdiv(@total))*100).round(2)}%" + " ...Computing presence score..." + ['/', '-', '\\'].sample
-    @counter += 1
-    deputies.each{|deputy| deputy.update(presence: (deputy.positions.where(votant: true).size.fdiv(deputy.positions.size)*100).round(0))}
+    deputies.each_with_index do |deputy, i|
+      deputy.update(presence: (deputy.positions.where(votant: true).size.fdiv(deputy.positions.size)*100).round(0))
+      print "\r#{(((i+1).fdiv(@total))*100).round(2)}%" + " ...Computing presence score..." + ['/', '-', '\\'].sample
+    end
   end
 end
