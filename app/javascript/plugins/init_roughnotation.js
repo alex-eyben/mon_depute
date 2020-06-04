3088199018d7f74e89280b7257461a42d1bb67fd
victor holl <v_h@me.com>
2020-06-04 16:15:59 +0200

    rough notation fix and getting rid of sandrine

--- app/javascript/plugins/init_roughnotation.js
+++ app/javascript/plugins/init_roughnotation.js
@@ -1,36 +1,38 @@
 import { annotate } from 'rough-notation';
 
 const initRoughnotation = () => {
-  const highlights = document.querySelectorAll('.highlight');
-  highlights.forEach((highlight) => {
-    const annotation = annotate(highlight, { type: 'highlight', color: '#FFF176' });
-    annotation.show();
-  });
-  const underlinesBlue = document.querySelectorAll('.underline-blue');
-  underlinesBlue.forEach((underline) => {
-    const annotation = annotate(underline, { type: 'underline', color: '#2196F3', padding: 3, strokeWidth: 3 });
-    annotation.show();
-  });
-  const underlinesRed = document.querySelectorAll('.underline-red');
-  underlinesRed.forEach((underline) => {
-    const annotation = annotate(underline, { type: 'underline', color: '#DB3F41', padding: 3, strokeWidth: 3 });
-    annotation.show();
-  });
-  const roughboxes = document.querySelectorAll('.roughbox');
-  roughboxes.forEach((box) => {
-    const annotation = annotate(box, { type: 'box', color: '#FFF176' });
-    annotation.show();
-  });
-  const followButton = document.querySelector(".follow")
-  const dropdownButton = document.querySelector(".dropdown-toggle")
-
-  if (followButton) {
-    followButton.addEventListener("click", (event) => {
-      console.log("coucou");
-      const annotation = annotate(dropdownButton, { type: 'box', color: '#FFF176' });
+  document.fonts.ready.then(function () {
+    const highlights = document.querySelectorAll('.highlight');
+    highlights.forEach((highlight) => {
+      const annotation = annotate(highlight, { type: 'highlight', color: '#FFF176' });
       annotation.show();
     });
-  }
+    const underlinesBlue = document.querySelectorAll('.underline-blue');
+    underlinesBlue.forEach((underline) => {
+      const annotation = annotate(underline, { type: 'underline', color: '#2196F3', padding: 3, strokeWidth: 3 });
+      annotation.show();
+    });
+    const underlinesRed = document.querySelectorAll('.underline-red');
+    underlinesRed.forEach((underline) => {
+      const annotation = annotate(underline, { type: 'underline', color: '#DB3F41', padding: 3, strokeWidth: 3 });
+      annotation.show();
+    });
+    const roughboxes = document.querySelectorAll('.roughbox');
+    roughboxes.forEach((box) => {
+      const annotation = annotate(box, { type: 'box', color: '#FFF176' });
+      annotation.show();
+    });
+    const followButton = document.querySelector(".follow")
+    const dropdownButton = document.querySelector(".dropdown-toggle")
+
+    if (followButton) {
+      followButton.addEventListener("click", (event) => {
+        console.log("coucou");
+        const annotation = annotate(dropdownButton, { type: 'box', color: '#FFF176' });
+        annotation.show();
+      });
+    }
+  });
 };
 
 export { initRoughnotation };
