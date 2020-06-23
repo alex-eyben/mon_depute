70ed77f8e6e04539df26561acd5fa434f82b92bd
Ugo Castellazi <ugo.castellazzi@gmail.com>
2020-06-23 17:18:58 +0200

    adding notes on next steps

--- app/javascript/components/like_position.js
+++ app/javascript/components/like_position.js
@@ -1,24 +1,15 @@
-// Toute la logie du like est dans le controller député. Devrait être dans le controller position ?
-// Ou alors cette route renvoie un JSON avec toutes les infos dont a besoin le JS.
-
-// créer un dataset qui contient le deputy.id et le position.id
-// créer un selecteur sur le like
-
-// quand le like est cliqué fetch sur deputies/positionId/like
-// si true rien ne change
-// si false passe en true et changement du inner HTML du boutton et du next sibling
-
-
-// quand le dislike est cliqué fetch sur deputies/positionId/like
-// si false rien ne change
-// si true passe en false et changement du inner HTML du boutton et du next sibling
-
 import { fetchWithToken } from "../utils/fetch_with_token";
 
 const likePosition = () => {
   const likeIcon = document.querySelectorAll(".fa-thumbs-up")
   const dislikeIcon = document.querySelectorAll(".fa-thumbs-down")
 
+
+  // si l'icone est un down on change le like en true. 
+  // on refresh toutes les icones de la carte après un click
+  // a l'intérieur de l'action update on a deux fetch : 
+  // un qui like et un qui va chercher le counter et le statut (true or false)
+  // et on update le html avec ça
   likeIcon.forEach((icon) => {
     const counter = icon.parentNode.nextSibling.nextSibling;
     icon.addEventListener('click', (event) => {
