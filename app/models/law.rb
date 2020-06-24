3262e755d657bf4e7e7c443e51dec8d8b733a9ff
Ugo Castellazi <ugo.castellazzi@gmail.com>
2020-06-24 15:50:18 +0200

    problems with initialize method and rails admin

--- app/models/law.rb
+++ app/models/law.rb
@@ -1,6 +1,15 @@
+require "nokogiri"
+require "open-uri"
+
 class Law < ApplicationRecord
   has_many :positions
   has_many :deputies, through: :positions
 
   acts_as_taggable_on :tags
+
+  # def initialize
+  #   raise
+  # end
+
+
 end
