484c5813a101d681cb72f2bffd7bc656c538c8c1
Ugo Castellazi <ugo.castellazzi@gmail.com>
2020-06-29 05:29:52 +0200

    laws appear chronologically, whatever the deputy position

--- app/controllers/deputies_controller.rb
+++ app/controllers/deputies_controller.rb
@@ -9,16 +9,22 @@ class DeputiesController < ApplicationController
     @tag = params[:tag]
     @participationRate = getParticipationRate(@deputy).fdiv(100)
     if @tag
-      positions = @deputy.positions.order(date: :desc)
+      positions = @deputy.positions.sort_by { |position| Law.find(position.law_id).last_status_update }.reverse
       @positions = positions.select { |position| position.law.tag_list.include? params[:tag] }
       @filteredParticipationRate = getParticipationRateFiltered(@deputy, @tag).fdiv(100)
     else
-      @positions = @deputy.positions.order(date: :desc)
+      @positions = @deputy.positions.sort_by { |position| Law.find(position.law_id).last_status_update }.reverse
     end
     @user = current_user
     @frondingRate = (100 - @deputy.fronding).fdiv(100)
     @topTags = getTopTags(5)
     @yearlyRevenue = @deputy.yearly_revenue / 1000
+  end
+
+  def orderPositions(positions)
+    laws = positions.map do |position|
+      Law.find(law_id).last_status_update
+    end 
   end
 
   # string.split(" ").reject{ |word| word == "-"}
