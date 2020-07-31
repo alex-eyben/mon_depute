da3fc116f63d9237fe9ddaa5b56539644745ee8f
Ugo Castellazi <ugo.castellazzi@gmail.com>
2020-07-31 12:03:39 +0200

    added Yarn dotenv

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,6 +1,10 @@
 import places from 'places.js';
+require('dotenv').config()
 
 const initAutocomplete = () => {
+  const key = process.env.ALGOLIA_API_KEY;
+  console.log(key);
+  console.log("hey");
   const addressInput = document.getElementById('location-search');
   if (addressInput) {
     places({ container: addressInput
