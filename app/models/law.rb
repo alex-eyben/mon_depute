73dc612cbbeb1a59006ab6a09708b44bb22853f9
Ugo Castellazi <ugo.castellazzi@gmail.com>
2020-08-04 18:32:11 +0200

    homepage showing last laws

--- app/models/law.rb
+++ app/models/law.rb
@@ -3,4 +3,8 @@ class Law < ApplicationRecord
   has_many :deputies, through: :positions
 
   acts_as_taggable_on :tags
+
+  def self.last_three
+    Law.order(last_status_update: :desc).first(3)
+  end
 end
