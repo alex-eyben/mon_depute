ead4eb3acbbdd2dcf57640489310c4738bc8ff63
Ugo Castellazi <ugo.castellazzi@gmail.com>
2021-04-20 12:21:02 +0200

    test by running jobs after law create

--- app/models/law.rb
+++ app/models/law.rb
@@ -2,7 +2,19 @@ class Law < ApplicationRecord
   has_many :positions, dependent: :destroy
   has_many :deputies, through: :positions
 
+  after_create :populate_law
+
+  acts_as_taggable_on :tags
+
   def self.last_three
     Law.order(last_status_update: :desc).first(3)
+  end
+
+  def populate_law
+    ImportPositionsJob.perform_now([self.scrutin_id])
+    AddTagsToLawJob.perform_now(self)
+    CountPositionsOnLawJob.perform_now(self)
+    GetFrondeurStatusJob.perform_now(Deputy.all)
+    GetPresenceScoreJob.perform_now(Deputy.all)
   end
 end
